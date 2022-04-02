a5eed18b4ff78668d66b96675ce35394b89b31b9
alireza kargar <imanesche@gmail.com>
2022-04-02 16:45:30 +0430

    added websocket

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alirezakargar1380/agar.io-golang
 
 go 1.17
+
+require github.com/gorilla/websocket v1.5.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func Reader(conn *websocket.Conn) {
+	for {
+		log.Println("nothing...")
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Client successfully connected...")
+	Reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/ws", wsEndpoint)
+}
 
 func main() {
 	fmt.Println("hello im backEnd agario")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
