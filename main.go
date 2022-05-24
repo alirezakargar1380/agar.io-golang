0cfec97b78af9b53ebaafca94bdc5093c1213ee2
alireza kargar <imanesche@gmail.com>
2022-05-24 21:09:18 +0430

    added jwt in main

--- main.go
+++ main.go
@@ -10,11 +10,55 @@ import (
 	"github.com/alirezakargar1380/agar.io-golang/app/api/routers"
 	"github.com/alirezakargar1380/agar.io-golang/app/databases"
 	"github.com/alirezakargar1380/agar.io-golang/app/socket"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-redis/redis"
 	"github.com/joho/godotenv"
 )
 
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
 func main() {
+	expirationTime := time.Now().Add(1 * time.Minute)
+	claims := &Claims{
+		Username: "alireza",
+		StandardClaims: jwt.StandardClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	fmt.Println(token)
+	var jwtKey = []byte("my_secret_key")
+	tokenString, errorsd := token.SignedString(jwtKey)
+	if errorsd != nil {
+		fmt.Println(errorsd)
+	}
+	fmt.Println(tokenString)
+
+	claims = &Claims{}
+	tknStr := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsaXJlemEiLCJleHAiOjE2NTM0MTAzNTd9.Tq5pEc4DVKaoGqsfWeapvdYWpE_E0Su8dLgjf2ulrg4"
+	tkn, errrrr := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if errrrr != nil {
+		if errrrr == jwt.ErrSignatureInvalid {
+			fmt.Println(http.StatusUnauthorized, "StatusUnauthorized")
+			return
+		}
+		fmt.Println(http.StatusBadRequest, "StatusBadRequest")
+		return
+	}
+	if !tkn.Valid {
+		fmt.Println(http.StatusUnauthorized, "StatusUnauthorized")
+		return
+	}
+	fmt.Println(claims.Username)
+
 	/*   .env file   */
 	err := godotenv.Load("config/config.env")
 	if err != nil {
